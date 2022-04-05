3735a677bb7aa1b398e5f62d5bc8e50bf317de67
Abhishek Kejriwal <abhishek.kejriwal@mapbox.com>
2022-04-05 11:33:17 -0700

    bump dependencies

--- android-auto/build.gradle.kts
+++ android-auto/build.gradle.kts
@@ -21,13 +21,13 @@ android {
 
 dependencies {
     api("androidx.car.app:app:1.1.0")
-    api("com.mapbox.navigation:android:2.4.0-beta.3")
+    api("com.mapbox.navigation:android:2.4.0-rc.1")
     api("com.mapbox.search:mapbox-search-android:1.0.0-beta.26")
     api("com.mapbox.search:mapbox-search-android-ui:1.0.0-beta.26")
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
@@ -38,7 +38,7 @@ dependencies {
     testImplementation("io.mockk:mockk:1.12.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
-    testImplementation("androidx.lifecycle:lifecycle-runtime-testing:2.4.0")
+    testImplementation("androidx.lifecycle:lifecycle-runtime-testing:2.4.1")
     testImplementation("androidx.arch.core:core-testing:2.1.0")
     testImplementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
 }
