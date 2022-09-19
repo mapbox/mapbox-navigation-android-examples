f8e46c8cfea223bb6271a57c759318cdfb2b6ca1
Abhishek Kejriwal <abhishek.kejriwal@mapbox.com>
2022-09-19 10:28:10 +0530

    demonstrate hiding selected views in free drive state

---
+++ app-preview/src/main/java/com/mapbox/navigation/examples/preview/dropinui/HideViewsInFreeDriveActivity.kt
@@ -0,0 +1,90 @@
+package com.mapbox.navigation.examples.preview.dropinui
+
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.Scene
+import com.mapbox.navigation.base.ExperimentalPreviewMapboxNavigationAPI
+import com.mapbox.navigation.core.MapboxNavigation
+import com.mapbox.navigation.core.lifecycle.MapboxNavigationObserver
+import com.mapbox.navigation.dropin.NavigationViewListener
+import com.mapbox.navigation.examples.preview.R
+import com.mapbox.navigation.examples.preview.databinding.MapboxActivityHideViewsFreeDriveBinding
+import com.mapbox.navigation.ui.base.lifecycle.UIBinder
+
+/**
+ * The example demonstrates how to hide `SpeedLimit`, `RoadNameLabel` and `ActionButtons` in free
+ * drive state but show in other states.
+ *
+ * Before running the example make sure you have put your access_token in the correct place
+ * inside [app-preview/src/main/res/values/mapbox_access_token.xml]. If not present then add
+ * this file at the location mentioned above and add the following content to it
+ *
+ * <?xml version="1.0" encoding="utf-8"?>
+ * <resources xmlns:tools="http://schemas.android.com/tools">
+ *     <string name="mapbox_access_token"><PUT_YOUR_ACCESS_TOKEN_HERE></string>
+ * </resources>
+ *
+ * The example uses replay location engine to facilitate navigation without physically moving.
+ *
+ * How to use the example:
+ * - Start the example
+ * - Grant the location permissions if not already granted
+ * - Long press on the map to select a destination
+ */
+@OptIn(ExperimentalPreviewMapboxNavigationAPI::class)
+class HideViewsInFreeDriveActivity : AppCompatActivity() {
+
+    private lateinit var binding: MapboxActivityHideViewsFreeDriveBinding
+    private val navigationStateListener = object : NavigationViewListener() {
+        override fun onFreeDrive() {
+            customizeViewBinders(shouldHide = true)
+        }
+
+        override fun onDestinationPreview() {
+            customizeViewBinders(shouldHide = false)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MapboxActivityHideViewsFreeDriveBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navigationView.api.routeReplayEnabled(true)
+
+        binding.navigationView.addListener(navigationStateListener)
+    }
+
+    private fun customizeViewBinders(shouldHide: Boolean) {
+        val viewBinder = if (shouldHide) EmptyBinder() else UIBinder.USE_DEFAULT
+        binding.navigationView.customizeViewBinders {
+            roadNameBinder = viewBinder
+            speedLimitBinder = viewBinder
+            actionButtonsBinder = viewBinder
+        }
+    }
+
+    /**
+     * EmptyBinder that can be used to hide a view.
+     */
+    @OptIn(ExperimentalPreviewMapboxNavigationAPI::class)
+    class EmptyBinder : UIBinder {
+        override fun bind(viewGroup: ViewGroup): MapboxNavigationObserver {
+            Scene.getSceneForLayout(
+                viewGroup,
+                R.layout.mapbox_empty_layout,
+                viewGroup.context,
+            ).enter()
+            return object : MapboxNavigationObserver {
+                override fun onAttached(mapboxNavigation: MapboxNavigation) {
+                    // No op for empty view binder
+                }
+
+                override fun onDetached(mapboxNavigation: MapboxNavigation) {
+                    // No op for empty view binder
+                }
+            }
+        }
+    }
+}
