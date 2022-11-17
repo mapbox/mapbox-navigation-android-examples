18ec1e46eefea3d76ec7f0533b67b32c321aab86
Brandan <104797391+brandanblevins@users.noreply.github.com>
2022-11-17 10:37:53 -0700

    Edit comments for CopilotViewModel kt

--- app-preview/src/main/java/com/mapbox/navigation/examples/preview/copilot/CopilotViewModel.kt
+++ app-preview/src/main/java/com/mapbox/navigation/examples/preview/copilot/CopilotViewModel.kt
@@ -7,8 +7,8 @@ import com.mapbox.navigation.core.MapboxNavigation
 import com.mapbox.navigation.core.lifecycle.MapboxNavigationObserver
 
 /**
- * [ViewModel] used in [CopilotActivity]. It handles [MapboxCopilot]'s lifecycle by
- * managing when to call [MapboxCopilot.start] / [MapboxCopilot.stop] APIs.
+ * [ViewModel] is used in [CopilotActivity] to handle [MapboxCopilot]'s lifecycle by
+ * managing when to call the [MapboxCopilot.start] / [MapboxCopilot.stop] API endpoints.
  *
  * Copilot is a [MapboxNavigationObserver], so it's tied to the [MapboxNavigation] lifecycle automatically.
  *
@@ -16,18 +16,19 @@ import com.mapbox.navigation.core.lifecycle.MapboxNavigationObserver
  * have the choice to enable it for your users (drivers). Depending on the use case, you can enable Copilot
  * for either all drivers (for example, during a pilot) or a subset of them.
  *
- * We recommend to tie [MapboxCopilot.start] and [MapboxCopilot.stop] APIs to app's lifecycle.
+ * We recommend connecting [MapboxCopilot.start] and [MapboxCopilot.stop] APIs to your app's lifecycle.
  *
  * WARNING: Mapbox Copilot is currently in public-preview. Copilot-related entities and APIs are currently marked
  * as [ExperimentalPreviewMapboxNavigationAPI] and subject to change.
  * These markings will be removed when the feature is generally available.
  *
- * Copilot is a library included in the Navigation SDK that Processes full-trip-trace longitude and
- * latitude data ("**Copilot**"). Copilot is turned off by default and optionally enabled by Customer at the
- * application developer level to improve feedback resolution. If Customer enables Copilot, Customer shall obtain
- * and maintain all necessary consents and permissions, including providing notice to and obtaining End
- * Users' affirmative express consent before any access or use of Copilot.
+ * Copilot is a library included in the Navigation SDK that processes full-trip-trace longitude and
+ * latitude data ("**Copilot**"). Copilot is turned off by default, and can be enabled by you at the
+ * application-developer level to improve feedback resolution. If you enable Copilot, your organization is responsible
+ * for obtaining and maintaining all necessary consents and permissions, including providing notice to and obtaining your end
+ * users' affirmative, expressed consent before any access or use of Copilot.
  */
+
 @OptIn(ExperimentalPreviewMapboxNavigationAPI::class)
 class CopilotViewModel : ViewModel() {
 
