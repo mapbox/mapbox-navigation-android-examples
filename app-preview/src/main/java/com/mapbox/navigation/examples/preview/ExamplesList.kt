b7dce836b85d864f23b88841353e5db17cac4415
dmitrii.samarian <dmitrii.samarian@mapbox.com>
2024-03-28 12:47:27 +0300

    Add example of rendering current location on the map

--- app-preview/src/main/java/com/mapbox/navigation/examples/preview/ExamplesList.kt
+++ app-preview/src/main/java/com/mapbox/navigation/examples/preview/ExamplesList.kt
@@ -2,16 +2,13 @@ package com.mapbox.navigation.examples.preview
 
 import android.content.Context
 import androidx.core.content.ContextCompat
-import com.mapbox.navigation.examples.preview.copilot.CopilotActivity
+import com.mapbox.navigation.examples.preview.location.ShowCurrentLocationActivity
 
 fun Context.examplesList() = listOf(
     MapboxExample(
-        ContextCompat.getDrawable(
-            this,
-            R.drawable.mapbox_screenshot_copilot
-        ),
-        getString(R.string.title_copilot),
-        getString(R.string.description_copilot),
-        CopilotActivity::class.java
+        ContextCompat.getDrawable(this, R.drawable.mapbox_ic_user_current_location),
+        getString(R.string.title_current_location),
+        getString(R.string.description_current_location),
+        ShowCurrentLocationActivity::class.java
     ),
 )
